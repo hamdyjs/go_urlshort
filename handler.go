0b3d0da14035d0380b837ec902752358d242b7bb
Ahmed Hamdy <hamdyjs@gmail.com>
2019-01-17 00:46:30 +0200

    Implement YAML handler

--- handler.go
+++ handler.go
@@ -1,6 +1,10 @@
-package gophercise2
+package urlshort
 
-import "net/http"
+import (
+	"net/http"
+
+	yamlV2 "gopkg.in/yaml.v2"
+)
 
 // MapHandler will return http.HandlerFunc
 func MapHandler(urlMap map[string]string, fallback http.Handler) http.HandlerFunc {
@@ -16,5 +20,33 @@ func MapHandler(urlMap map[string]string, fallback http.Handler) http.HandlerFun
 
 // YAMLHandler will return http.HandlerFunc
 func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return nil, nil
+	parsedYAML, err := parseYAML(yaml)
+	if err != nil {
+		return nil, err
+	}
+	urlMap := buildMap(parsedYAML)
+	return func(w http.ResponseWriter, r *http.Request) {
+		path, ok := urlMap[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}, nil
+}
+
+func parseYAML(yaml []byte) (result []map[string]string, err error) {
+	err = yamlV2.Unmarshal(yaml, &result)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+func buildMap(yaml []map[string]string) map[string]string {
+	urlMap := make(map[string]string)
+	for _, entry := range yaml {
+		urlMap[entry["path"]] = entry["url"]
+	}
+	return urlMap
 }
